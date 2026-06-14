00914ba3afcaa24b6796fe45b8c375a5162a36bd
67130043-tech <67130043@dpu.ac.th>
2026-06-14 21:38:29 +0700

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -16,15 +16,27 @@ class PredictRequest(BaseModel):
 def root():
     return {"status": "ok", "service": "Auto Engagement AI Processing API", "endpoint": "/predict"}
 
-@app.post("/predict")
-def predict(req: PredictRequest):
-    return predict_message(
-        user_id=req.user_id,
-        message=req.message,
-        channel=req.channel,
-        display_name=req.display_name,
-        source=req.source,
-    )
+#@app.post("/predict")
+#def predict(req: PredictRequest):
+#    return predict_message(
+#        user_id=req.user_id,
+#        message=req.message,
+#        channel=req.channel,
+#        display_name=req.display_name,
+#        source=req.source,
+#    )
+
+@app.post("/make/test")
+def make_test(request: PredictRequest):
+    return {
+        "user_id": request.user_id,
+        "message": request.message,
+        "sentiment": "Negative",
+        "category": "Delivery",
+        "segment": "Regular",
+        "action": "Apology",
+        "reply_message": "ขออภัยในความไม่สะดวกค่ะ ทางร้านจะรีบตรวจสอบให้ทันทีค่ะ"
+    }
 
 # endpoint นี้ตั้งชื่อให้ Make จำง่าย
 @app.post("/make/predict")
